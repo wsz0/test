c0c473b94fc083f0d057bde5c09a9b5d1f2465f0
wsz <1260266087@qq.com>
2021-06-30 16:34:46 +0800

    1

--- UniApp/Document/fisrt/pages/index/index.vue
+++ UniApp/Document/fisrt/pages/index/index.vue
@@ -2,10 +2,14 @@
 	<view class="container">
 		
   <view class="uni-flex uni-row">
+				<button @click="get">aaa</button>
                 <view class="box" style="background-color: #007AFF;">A</view>
                 <view class="box" style="background-color: #4CD964;">B</view>
-                <view class="box" style="background-color: #F0AD4E;">C</view>
+                <view class="box" style="background-color: #F0AD4E;" name="a">{{text1}}</view>
             </view>
+			<view>
+				<input type="" />
+			</view>
 		<uni-link :href="href" :text="href"></uni-link>
 	</view>
 </template>
@@ -13,12 +17,46 @@
 <script>
 	export default {
 		data() {
+			
 			return {
+				text1:'',
 				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+				,
+
 			}
 		},
+		onLoad(){
+			let that=this
+					uni.request({
+							url: 'http://localhost:52325/api/Aa/sendHello',
+							method: 'GET',
+							
+							success(res){
+								//console.log(res);
+								//that.text1=res.data;
+								//console.log(this);
+								console.log(that);
+								
+							}
+						});
+			
+		},
 		methods: {
-
+				get(){
+					let that=this
+					uni.request({
+							url: 'http://localhost:52325/api/Aa/sendHello',
+							method: 'GET',
+							
+							success(res){
+								//console.log(res);
+								that.text1=res.data;
+								//console.log(this);
+								console.log(that);
+								
+							}
+						});
+				}
 		}
 	}
 </script>
